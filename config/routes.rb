dddaafe4b81a01770d549b0c58f5c5d0e9e9132f
MbF-Arun <d.arun@madebyfire.com>
2018-02-13 17:47:26 +0530

    devise session had mbeen commited

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "templates#index"
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    delete 'logout', to: 'devise/sessions#destroy'
+
+  end
+
 end
